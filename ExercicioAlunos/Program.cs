981b8fc3124eb8f428e9858c76294fcafef7ae75
Giovana Campos <100374082+ProjectCampos@users.noreply.github.com>
2023-03-15 15:20:40 -0300

    Add files via upload

---
+++ ExercicioAlunos/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicioAlunos;
+class Program
+{ 
+     static void Main(string[] args){
+       Aluno aluno1 = new Aluno();
+        
+Console.WriteLine("Digite seu nome: ");
+aluno1.nome = Console.ReadLine();
+Console.WriteLine("Digite sua primeira nota: ");
+aluno1.nota1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite sua segunda nota: ");
+aluno1.nota2 = Convert.ToDouble(Console.ReadLine());
+
+aluno1.mensagem();
+
+
+    }  
+      
+}
