4e474c6fdba3a6b1efa20ea8d7d58aa9e62c70eb
Giovana Campos <100374082+ProjectCampos@users.noreply.github.com>
2023-03-29 14:58:05 -0300

    Add files via upload

---
+++ metodo_construtor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace metodo_construtor;
+class Program
+{
+static void Main(string[] args)
+    {
+    Produto p1 = new Produto("abc", 10);
+
+
+
+           p1.MostraDados();
+    
+    }
+}
